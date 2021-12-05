e1cb31269a7fb47b196938e1844d99dc87e195a4
Jrjesus18 <juniorlopez3@live.com>
2021-12-04 21:24:43 -0500

    Update

---
+++ src/routers/AppRouters.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route
+} from "react-router-dom";
+import Navbar from '../components/Navbar'
+import Estudiantes from "../components/Estudiantes";
+
+
+export default class AppRouters extends Component {
+    render() {
+        return (
+            <div>
+                <Router>
+                    <Navbar />
+                    <Routes>
+                        <Route exact path="/Estudiantes" element={<Estudiantes/>} />
+                    </Routes>
+                </Router>
+            </div>
+        )
+    }
+}
+
